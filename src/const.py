7f213b66e85c976254cb8786b28886d118ef6c96
Emma Sun <emma.sun@thoughtworks.com>
2020-03-14 15:38:39 +0800

    implement POST route for prediction

--- src/const.py
+++ src/const.py
@@ -42,3 +42,21 @@ category_columns = [
     "bed_type",
     "cancellation_policy"
 ]
+
+# used to extract columns in the correct order from post request json object
+column_order = ['host_id', 'host_response_time', 'host_response_rate',
+                'host_is_superhost', 'host_verifications', 'host_has_profile_pic',
+                'host_identity_verified', 'neighbourhood_group_cleansed', 'latitude',
+                'longitude', 'is_location_exact', 'property_type', 'room_type',
+                'accommodates', 'bathrooms', 'bedrooms', 'beds', 'bed_type',
+                'amenities', 'security_deposit', 'cleaning_fee',
+                'guests_included', 'extra_people', 'has_availability',
+                'availability_365', 'number_of_reviews', 'review_scores_rating',
+                'review_scores_accuracy', 'review_scores_cleanliness',
+                'review_scores_checkin', 'review_scores_communication',
+                'review_scores_location', 'review_scores_value', 'instant_bookable',
+                'cancellation_policy', 'require_guest_profile_picture',
+                'require_guest_phone_verification', 'calculated_host_listings_count',
+                'calculated_host_listings_count_entire_homes',
+                'calculated_host_listings_count_private_rooms',
+                'calculated_host_listings_count_shared_rooms', 'reviews_per_month']
